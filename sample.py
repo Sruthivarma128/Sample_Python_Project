95038fa5f4065ad03e39d3a03a05c7032ea73b81
Sruthivarma128 <54289344+Sruthivarma128@users.noreply.github.com>
2020-01-28 16:27:54 -0500

    Create sample script

---
+++ sample.py
@@ -0,0 +1,14 @@
+
+def about_me(your_name):
+    print("The wise {} loves Python.".format(your_name))            
+
+def HW():
+    print("Hello World!")
+
+def Both():
+    HW()
+    about_me("Ab")
+
+Both()
+F  = open(r"C:\Users\AMRABET\Documents\VSC\HW\a.txt", "a")
+F.write("ok\n")
